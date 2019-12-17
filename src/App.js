4ad4ac34ed273a6446a15391208bd284285b70e0
jonathanboss17 <jtboss47@gmail.com>
2019-12-17 15:20:32 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+// **
+// allows us to specify which data we are listening to (through mapStateToProps), and which component we are providing the data
+import { connect } from 'react-redux'; 
+
 import './App.css';
 
 class App extends Component {
 
   handleOnClick() {
+    // dispatch is automatically provided by 'connect'
     this.props.dispatch({
       type: 'INCREASE_COUNT',
     });
@@ -21,4 +26,18 @@ class App extends Component {
   }
 };
 
-export default App;
+// specify which slice of the state we want to provide to our component
+// want to provide 'state.items' and allow our component to have access to them through a prop called items
+// returns an items prop that is used in App.js
+const mapStateToProps = (state) => {
+  return { items: state.items };  
+}
+
+// for the above function
+// ...***App component can access that state with the 'this.props.items' (ie what's returned from mapStateToProps)
+
+// listens to each change in the state that occurs
+// also specifies which component (App) that we're providing the data to
+export default connect(mapStateToProps)(App);
+
+// the entire connect() method return s a new component that looks like the App component, but receives the correct data
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
+
+import { Provider } from 'react-redux'; 
+
 import shoppingListItemReducer from './reducers/shoppingListItemReducer';
 import App from './App';
 import './index.css';
@@ -11,6 +14,9 @@ const store = createStore(
 );
 
 ReactDOM.render(
-  <App store={store}/>,
+  // Provider ensures that our entire React application can potentially access data from the store
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
